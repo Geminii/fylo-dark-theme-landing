4a81dc3126d126b3c137f394e5b24e4c0c1ea7c5
Jimmy Jouanne <jimmy@liberkeys.com>
2022-01-09 13:46:16 +0100

    feat: add typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
     "./index.html"
   ],
   theme: {
-    extend: {},
+    colors: {
+      'dark-blue': 'var(--dark-blue)',
+      'dark-blue-bg': 'var(--dark-blue-bg)',
+      'dark-blue-footer': 'var(--dark-blue-footer)',
+      'dark-blue-testimonials': 'var(--dark-blue-testimonials)',
+      'cyan': 'var(--cyan)',
+      'blue': 'var(--blue)',
+      'light-red': 'var(--light-red)',
+      'white': 'var(--white)',
+    },
+    fontFamily: {
+      'raleway': ['Raleway', 'sans-serif'],
+      'open-sans': ['Open sans', 'sans-serif'],
+    }
   },
   plugins: [],
 }
